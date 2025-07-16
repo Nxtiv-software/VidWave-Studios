8c5cc95f4371f2c4282801cfce5e6edb6fcdeeee
bojitha001 <nawarathnabojitha@gmail.com>
2025-07-16 22:36:48 +0530

    init commit

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -3,12 +3,14 @@ import NavBar from "./components/NavBar";
 import Testimonials from "./components/Testimonials";
 import Home from "./ui/Home/Home";
 import Stories from "./ui/Home/Stories";
+import StoriesWeTell from "./ui/Home/StoriesWeTell";
 
 function MainPage() {
   return (
     <div>
       <NavBar />
       <Home />
+      <StoriesWeTell/>
       <Stories />
       <Testimonials />
       <Footer />
---
+++ src/ui/Home/StoriesWeTell.jsx
@@ -0,0 +1,21 @@
+import img1 from "../../../public/s-1.png";
+import img2 from "../../../public/s-2.png";
+import img3 from "../../../public/s-3.png";
+import img4 from "../../../public/s-4.png";
+
+const images = [{ img: img1 }, { img: img2 }, { img: img3 }, { img: img4 }];
+
+function StoriesWeTell() {
+  return (
+    <div className="flex flex-col items-center justify-center pb-40">
+      <h3 className="mb-10 text-4xl text-white">Stories We Tell</h3>
+      <div className="h-[50vh] flex gap-20 items-center">
+        {images.map((img) => (
+          <img className="h-60 w-60 object-cover" src={img.img} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default StoriesWeTell;
