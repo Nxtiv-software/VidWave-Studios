69d79373129bc30f43f55e57f8166f0157b4614a
bojitha001 <nawarathnabojitha@gmail.com>
2025-07-16 22:16:47 +0530

    init commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,9 @@
 function Button({ children }) {
-  return <button className="bg-gray-300 px-2 py-1 rounded-sm mt-5">{children}</button>;
+  return (
+    <button className="border-none border-white px-4 py-2 rounded-sm mt-5 text-white shadow-[0_0_10px_rgba(255,255,255,0.5)]">
+      {children}
+    </button>
+  );
 }
 
 export default Button;
